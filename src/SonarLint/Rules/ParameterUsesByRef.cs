39759e7965cbb03dc4359bc28658cd8c55bfb876
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-11-02 14:02:34 +0100

    Issue #490 Parameters should not be passed by reference

---
+++ src/SonarLint/Rules/ParameterUsesByRef.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2015 SonarSource
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarLint.Common;
+using SonarLint.Common.Sqale;
+using SonarLint.Helpers;
+
+namespace SonarLint.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.VisualBasic)]
+    [SqaleConstantRemediation("30min")]
+    [SqaleSubCharacteristic(SqaleSubCharacteristic.LogicReliability)]
+    [Rule(DiagnosticId, RuleSeverity, Title, IsActivatedByDefault)]
+    [Tags(Tag.Pitfall)]
+    public class ParameterUsesByRef : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S2361";
+        internal const string Title = "Parameters should not be passed by reference";
+        internal const string Description =
+            "Passing parameters by reference requires developers to understand the subtle differences between reference and value " +
+            "types. It is preferable to avoid passing parameters by reference when possible.";
+        internal const string MessageFormat = "Refactor the code and pass \"{0}\" by value instead.";
+        internal const string Category = Constants.SonarLint;
+        internal const Severity RuleSeverity = Severity.Major;
+        internal const bool IsActivatedByDefault = false;
+
+        internal static readonly DiagnosticDescriptor Rule =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category,
+                RuleSeverity.ToDiagnosticSeverity(), IsActivatedByDefault,
+                helpLinkUri: DiagnosticId.GetHelpLink(),
+                description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                c =>
+                {
+                    var parameter = (ParameterSyntax)c.Node;
+                    if (parameter.Modifiers.Any(m => m.IsKind(SyntaxKind.ByRefKeyword)))
+                    {
+                        c.ReportDiagnostic(
+                            Diagnostic.Create(Rule, parameter.Identifier.GetLocation(), parameter.Identifier.Identifier.ValueText));
+                    }
+                },
+                SyntaxKind.Parameter);
+        }
+    }
+}
---
+++ src/Tests/SonarLint.UnitTest/TestCases/ParameterUsesByRef.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+    Sub Foo(ByRef result As Integer, ' Noncompliant
+            ByRef result2 As Integer, ' Noncompliant
+            ByVal result3 As Integer)
+        result = 42
+    End Sub
+
+    Function Foo2() As Integer        ' Compliant
+        Return 42
+    End Function
+
+    Sub Main()
+        Dim result As Integer
+        Foo(result)
+        Console.WriteLine(result)
+    End Sub
+End Module
